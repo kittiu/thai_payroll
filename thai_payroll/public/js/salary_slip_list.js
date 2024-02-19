b49168fc9e22cecbf4cc5eacf33d44f7257ab85c
Kitti U <kittiu@gmail.com>
2024-02-20 00:36:05 +0700

    Use extend_listview_event

--- thai_payroll/public/js/salary_slip_list.js
+++ thai_payroll/public/js/salary_slip_list.js
@@ -1,30 +1,23 @@
-frappe.listview_settings["Salary Slip"] = {
+function extend_listview_event(doctype, event, callback) {
+    if (!frappe.listview_settings[doctype]) {
+        frappe.listview_settings[doctype] = {};
+    }
 
-	onload: function(listview) {
+    const old_event = frappe.listview_settings[doctype][event];
+    frappe.listview_settings[doctype][event] = function (listview) {
+        if (old_event) {
+            old_event(listview);
+        }
+        callback(listview);
+    }
+}
 
-		// Override apps/hrms/hrms/payroll/doctype/salary_slip/salary_slip_list.js
-		if (!has_common(frappe.user_roles, ["Administrator", "System Manager", "HR Manager", "HR User"])) return;
-
-		listview.page.add_menu_item(__("Email Salary Slips"), () => {
-			if (!listview.get_checked_items().length) {
-				frappe.msgprint(__("Please select the salary slips to email"));
-				return;
-			}
-
-			frappe.confirm(__("Are you sure you want to email the selected salary slips?"), () => {
-				listview.call_for_selected_items("hrms.payroll.doctype.salary_slip.salary_slip.enqueue_email_salary_slips");
-			});
-		});
-		// --
-	
-		// Addition
-		listview.page.add_action_item(__("Withholding Tax Cert Employee"), ()=>{
-			erpnext.bulk_transaction_processing.create(
-				listview,
-				"Salary Slip",
-				"Withholding Tax Cert Employee"
-			);
-		});
-	}
-
-};
+extend_listview_event("Salary Slip", "onload", function (listview) {
+	listview.page.add_action_item(__("Withholding Tax Cert Employee"), ()=>{
+		erpnext.bulk_transaction_processing.create(
+			listview,
+			"Salary Slip",
+			"Withholding Tax Cert Employee"
+		);
+	});
+});
