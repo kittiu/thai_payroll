5e404c392e704790b39612144b5d135680801c01
Kitti U <kittiu@gmail.com>
2024-05-05 14:42:10 +0700

    Init PND1a Report

---
+++ thai_payroll/thai_payroll/report/pnd1a/pnd1a.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["PND1a"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			width: "100px",
+			reqd: 1,
+		},
+		{
+			fieldname: "payroll_period",
+			label: __("Payroll Period"),
+			fieldtype: "Link",
+			options: "Payroll Period",
+			reqd: 1,
+			on_change: () => {
+				var period = frappe.query_report.get_filter_value("payroll_period");
+				if (period) {
+					frappe.db.get_value("Payroll Period", period, ["start_date", "end_date"], function (value) {
+						frappe.query_report.set_filter_value("from_date", value["start_date"]);
+						frappe.query_report.set_filter_value("to_date", value["end_date"]);
+					});
+				} else {
+					frappe.query_report.set_filter_value("from_date", "");
+					frappe.query_report.set_filter_value("to_date", "");
+				}
+			},
+		},
+		{
+			fieldname: "from_date",
+			label: __("From"),
+			fieldtype: "Date",
+			reqd: 1,
+			width: "100px",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To"),
+			fieldtype: "Date",
+			reqd: 1,
+			width: "100px",
+		},
+	],
+};
