f046edb276cad790b56d3441b6a3462bee204a54
Kitti U <kittiu@gmail.com>
2024-09-06 12:55:58 +0700

    ENH: Use hooks.py to add bulk_transaction

--- thai_payroll/__init__.py
+++ thai_payroll/__init__.py
@@ -1,8 +1,1 @@
 __version__ = "0.0.1"
-
-
-# Monkey patching
-# ------------------
-import erpnext.utilities.bulk_transaction as bt
-from thai_payroll.custom.bulk_transaction import task
-bt.task = task
